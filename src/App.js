6e32d0861db7c024d478aafb0f573afeb6926532
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-08-17 02:38:52 -0400

    Setup static components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { useState } from 'react';
+
 function App() {
-  return <div className='App'></div>;
+  const [markdown, setMarkdown] = useState('## Markdown Preview');
+
+  return (
+    <main>
+      <section className='markdown'>
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={e => setMarkdown(e.target.value)}
+        />
+        <article className='result'>{markdown}</article>
+      </section>
+    </main>
+  );
 }
 
 export default App;
